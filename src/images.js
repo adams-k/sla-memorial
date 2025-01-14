f73ebe4dae0513c6f70a0fb7d0d500235532baa9
Kyle <kyl.adams4@gmail.com>
2025-01-13 22:49:32 -0500

    Added back amplify.yml

--- src/images.js
+++ src/images.js
@@ -4,8 +4,8 @@ import image3 from './assets/image3.jpeg';
 import image4 from './assets/image4.jpeg';
 import image5 from './assets/image5.jpeg';
 import image6 from './assets/image6.jpeg';
-import 
+
 
 export {
-    image0, image1, image2, image3, image4, image5, image6
+    image0, image1, image3, image4, image5, image6
 }
