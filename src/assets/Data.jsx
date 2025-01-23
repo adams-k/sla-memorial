a952d4b7a18c126745eb089d8e17ff97a890ee19
Kyle <kyl.adams4@gmail.com>
2025-01-22 23:09:11 -0500

    Small updates

--- src/assets/Data.jsx
+++ src/assets/Data.jsx
@@ -1,9 +1,9 @@
 export const navData = [
     ['home', 'Home', '/'],
     ['upcoming', 'Upcoming Events', '/upcoming'],
-    ['news', 'News', 'news'],
-    ['past', 'Past Events', 'past'],
-    ['sponsors', 'Sponsors', 'sponsors']
+    ['news', 'News', '/news'],
+    ['past', 'Past Events', '/past'],
+    ['sponsors', 'Sponsors', '/sponsors']
 ]
 
 export const sponsorsMap = [
